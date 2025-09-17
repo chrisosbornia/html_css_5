751ace777fc3a836215ffb655f374c1eba227384
Chris Osborn <k0099588@student.kirkwood.edu>
2025-09-17 16:10:09 +0000

    2025-09-17 demo project 2 changes

---
+++ demo-project/scripts/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+
+    const mobileNav = $('#mobile-nav');
+    const headerBottomNav = $('#header-bottom-nav');
+
+        $(window).resize()
+
+    if($(window).width() >= 992) {
+        headerBottomNav.css({'display': 'block'});
+    } else {
+        headerBottomNav.css({'display': 'none'});
+    }
+
+    mobileNav.click(() => headerBottomNav.toggle());
+
+
+    $(window).resize(function() {
+        if($(window).width() >= 992) {
+            headerBottomNav.css({'display': 'block'});
+        } else {
+            headerBottomNav.css({'display': 'none'});
+        }
+    });
+});
